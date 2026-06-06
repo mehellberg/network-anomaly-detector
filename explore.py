a552f80a08c98505a97a1aea04aeacdbfefc7a1d
mehellberg <melvinmed26@gmail.com>
2026-06-06 17:18:08 +0200

    Add packet parser with protocol and timestamp

---
+++ explore.py
@@ -0,0 +1,30 @@
+from scapy.all import *
+import pandas as pd
+
+paket = rdpcap("C:/Users/melvi/Documents/VSC/Projekt/network-anomaly-detector/capture.pcapng")
+
+rader = []
+
+for p in paket:
+    if p.haslayer("IP"):
+        if p.haslayer("TCP"):
+            protokoll = "TCP"
+        elif p.haslayer("UDP"):
+            protokoll = "UDP"
+        else:
+            protokoll = "Annat"
+        rad = {
+            "tid": float(p.time),
+            "src_ip": p["IP"].src,
+            "dst_ip": p["IP"].dst,
+            "storlek": len(p),
+            "protokoll": protokoll
+        }
+        rader.append(rad)
+
+df = pd.DataFrame(rader)
+df["tid"] = pd.to_datetime(df["tid"].astype(float), unit="s")
+
+print(df.head(10))
+print(df["protokoll"].value_counts())
+print(df["dst_ip"].value_counts().head(5))
